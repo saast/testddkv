5ea51e937ffcb89096a730bd8dfb21793a0dc975
Silver Samma <silver.samma@gmail.com>
2015-10-22 16:06:25 +0300

    First test

--- ekl/settings.py
+++ ekl/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'ekl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ekl',
+        'USER': 'postgres',
+        'PASSWORD': 'postgrespass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        browser.quit()
+
+
+    def test_page_titles(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('EKL', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
 
